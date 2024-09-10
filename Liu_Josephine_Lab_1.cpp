dda328d14043be6e51bebb79622a16c3d3f08e40
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-09-10 16:52:22 -0400

    dwasdasda

---
+++ Liu_Josephine_Lab_1.cpp
@@ -0,0 +1,43 @@
+//Josephine Liu
+//Lab 1
+//9/10/2024
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "#################################################################################" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                     #" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "#       Press the key combination Ctrl + Shift + Escape                         #" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "#       Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "#       Type “Task Manager” in the Windows Start menu search                    #" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "#                                                                               #" << endl;
+    cout << "#################################################################################" <<endl;
+    //cout is used to print the stuff you type and send it into output. endl is used to make a new line like enter but you cannot you it by itself like "cout << endl"
+    return 0;
+}
+
+/*
+[Running] cd "c:\Users\college\Documents\GitHub\lab-1-cout-statements-jo-svg\" && g++ Liu_Josephine_Lab_1.cpp -o Liu_Josephine_Lab_1 && "c:\Users\college\Documents\GitHub\lab-1-cout-statements-jo-svg\"Liu_Josephine_Lab_1
+#################################################################################
+#                                                                               #
+#                                                                               #
+# Ways to access the Task Manager on your Windows computer:                     #
+#                                                                               #
+#       Press the key combination Ctrl + Shift + Escape                         #
+#                                                                               #
+#       Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+#                                                                               #
+#       Type “Task Manager” in the Windows Start menu search                    #
+#                                                                               #
+#                                                                               #
+#################################################################################
+
+[Done] exited with code=0 in 1.654 seconds
+*/
